741a2956b87b8ddba45e9fba1bba65970d78e9a0
BruceWang <wangjinghua1215225@163.com>
2015-03-17 19:21:41 +0800

    完善相应的log函数

--- log.php
+++ log.php
@@ -1,14 +1,43 @@
 <?php 
 	/**
      * 日志生成函数 Author:WJH
-     * @param string $log 要在日志中显示的字符串
+     * @param string $log 要在日志中显示的信息
      * @param string $filePrefix 日志文件名的前缀
+	 * @param string $fileSuffix 日志文件名的后缀
+	 * @param return -3 打开文件失败 -2 写入文件失败 -1 关闭文件失败 0 成功
      */
-	function AddLog($log=''){
+	
+	function AddLog($log='',$filePrefix='',$fileSuffix='.log',$time='day'){
         $time=date('Y-m-d H:i:s',mktime());
-        $filename=date('YmdH',mktime()).'.log';
+		if($time=='year'){
+			$period=date('Y',mktime());
+		}elseif($time=='month'){
+			$period=date('Ym',mktime());
+		}elseif($time=='hour'){
+			$period=date('YmdH',mktime());
+		}elseif($time=='minute'){
+			$period=date('YmdHi',mktime());
+		}elseif($time=='second'){
+			$period=date('YmdHis',mktime());
+		}else{
+			$period=date('Ymd',mktime());
+		}
+        $filename=$filePrefix.$period.$filePrefix;
         $fp=fopen($filename,'a');
-        fwrite($fp,$time."\n".$log."\n");
-        fclose($fp);
+		if($fp){
+			$wr=fwrite($fp,$time."\n".$log."\n");
+			if($wr){
+				$close=fclose($fp);
+				if($close){
+					return 1;
+				}else{
+					return -1;
+				}
+			}else{
+				return -2;
+			}
+		}else{
+			return -3;
+		}
     }
 ?>
